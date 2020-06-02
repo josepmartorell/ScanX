1073b2461792e4aabb8a3a813d13b801bb25ef66
josepmartorell <martorelljosep>
2020-06-02 21:50:40 +0200

    api

--- ScanX_lib/api.py
+++ ScanX_lib/api.py
@@ -54,7 +54,25 @@ def run(config):
     # --------------------------------------------------------------------------
     # if version_info < 3:
     #     raise RuntimeError("You need Python 3.x or higher to run ScanX")
-
     # --------------------------------------------------------------------------
-    # INSERT YOUR CODE HERE  # TODO
+    from nmap import nmap
+    nm = nmap.PortScanner()
+    nm.scan('127.0.0.1', '21-443')
+    for host in nm.all_hosts():
+        print('----------------------------------------------------')
+        print('Host : %s (%s)' % (host, nm[host].hostname()))
+        print('State : %s' % nm[host].state())
+
+        for proto in nm[host].all_protocols():
+            print('----------')
+            print('Protocol : %s' % proto)
+
+            lport = nm[host][proto].keys()
+            lport.sort()
+            for port in lport:
+                print('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+
+    print('----------------------------------------------------')
+    # print result as CSV
+    print(nm.csv())
     # --------------------------------------------------------------------------
