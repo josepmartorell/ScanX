da9d931324d68dfaf33e77760c6c4d4462a95d93
josepmartorell <martorelljosep>
2020-06-03 04:01:19 +0200

    ping scan

--- ScanX_lib/api.py
+++ ScanX_lib/api.py
@@ -55,12 +55,12 @@ def run(config):
     # if version_info < 3:
     #     raise RuntimeError("You need Python 3.x or higher to run ScanX")
     # --------------------------------------------------------------------------
+    print('----------------------------------------------------')
     from nmap import nmap
     scanner = nmap.PortScanner()
     print("Nmap Version: ", scanner.nmap_version())
     scanner.scan('127.0.0.1', '21-443')
     for host in scanner.all_hosts():
-        print('----------------------------------------------------')
         print('Host : %s (%s)' % (host, scanner[host].hostname()))
         print('State : %s' % scanner[host].state())
         if scanner[host].has_tcp(22):
@@ -81,6 +81,10 @@ def run(config):
             lport.sort()
             for port in lport:
                 print('port : %s\tstate : %s' % (port, scanner[host][proto][port]['state']))
-
+        print('Ping Scan NETWORK ...')
+        scanner.scan(hosts='192.168.0.0/24', arguments='-n -sP -PE -PA21,23,80,3389')
+        hosts_list = [(x, scanner[x]['status']['state']) for x in scanner.all_hosts()]
+        for host, status in hosts_list:
+            print('{0}:{1}'.format(host, status))
     print('----------------------------------------------------')
     # --------------------------------------------------------------------------
